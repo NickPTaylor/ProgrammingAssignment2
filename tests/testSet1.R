4543c3d174b46a93d56bf956f3606fd61ca696a5
Nick <nick.monkey@googlmail.com>
2014-06-22 11:01:50 +0100

    First commit for Assignment 2

---
+++ tests/testSet1.R
@@ -0,0 +1,9 @@
+################################################################################
+#
+#  Unit Testing    
+#
+#  This is the unit test suite for cachematrix.R
+#
+################################################################################
+
+context("Tests for makeCacheMatrix function")
